cf5ff8f2e377b4bfca2a7e9bd304f251031b9c7d
yonathan-boop <yonathan-boop@users.noreply.github.com>
2026-09-18 09:45:46 +0700

    Absensi 17-18 September 2026 (PG): Hans 17-I, Stevano 17-S, Eric 17-S & 18-S

--- COMMON/scripts/absensi.py
+++ COMMON/scripts/absensi.py
@@ -7,7 +7,7 @@ import difflib
 
 FILE_PATH = os.environ.get(
     "ABSENSI_FILE",
-    "C:/Users/yonat/OneDrive/Desktop/memory/PC-06/docs/Absensi T.P 2026-2027/Absensi 16 September 2026 Wednesday 11_51_44.xlsx",
+    "C:/Users/yonat/OneDrive/Desktop/memory/PC-06/docs/Absensi T.P 2026-2027/Absensi 18 September 2026 Friday 09_45_34.xlsx",
 )
 
 KELAS_MAP = {
